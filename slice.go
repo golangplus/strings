6dcf9178c7802879b5470c71cb0b9051ba7177b7
David Deng <daviddengcn@gmail.com>
2015-07-30 00:02:23 -0700

    ADD SliceInsert

--- slice.go
+++ slice.go
@@ -7,3 +7,43 @@ func SliceRemove(s []string, index int) []string {
 	s[len(s)-1] = ""
 	return s[:len(s)-1]
 }
+
+// SliceInsert inserts some elements at index-th position and resturns the result slice.
+func SliceInsert(s []string, index int, e ...string) []string {
+	n := len(e)
+	if n == 0 {
+		// Nothing to insert
+		return s
+	}
+
+	l := len(s)
+	if index == l {
+		return append(s, e...)
+	}
+
+	// Make room for inserted elements.
+	if cap(s) >= l+n {
+		s = s[:l+n]
+		copy(s[index+n:], s[index:])
+		copy(s[index:], e)
+		return s
+	}
+
+	if n < len(s)-index {
+		// ...**** + ooo ==> ...ooo$$$$
+		s = append(s, s[l-n:]...)
+		// ...*...$$$
+		copy(s[index+n:l], s[index:])
+		// ......$$$$
+		copy(s[index:], e)
+		// ...ooo$$$$
+		return s
+	}
+
+	// ...*** + oooo ==> ...oooo$$$
+	s = append(append(s, e[l-index:]...), s[index:]...)
+	// ......o$$$
+	copy(s[index:], e[:l-index])
+	// ...oooo$$$
+	return s
+}
--- slice_test.go
+++ slice_test.go
@@ -9,3 +9,10 @@ import (
 func TestSliceRemove(t *testing.T) {
 	assert.Equal(t, "SliceRemove([1 2 3], 1)", SliceRemove([]string{"1", "2", "3"}, 1), []string{"1", "3"})
 }
+
+func TestSliceInsert(t *testing.T) {
+	assert.Equal(t, "SliceInsert([1 2 3], 3, 5)", SliceInsert([]string{"1", "2", "3"}, 3, "5"), []string{"1", "2", "3", "5"})
+	assert.Equal(t, "SliceInsert([1 2 3], 1, 5)", SliceInsert([]string{"1", "2", "3"}, 1, "5"), []string{"1", "5", "2", "3"})
+	assert.Equal(t, "SliceInsert([1 2 3], 1, 5, 6)", SliceInsert([]string{"1", "2", "3"}, 1, "5", "6"), []string{"1", "5", "6", "2", "3"})
+	assert.Equal(t, "SliceInsert([1 2], 1, 5, 6)", SliceInsert([]string{"1", "2"}, 1, "5", "6"), []string{"1", "5", "6", "2"})
+}
